9187e54124678610874f1a9bad73bc39835c724a
Ben Rucker <brucker117@gmail.com>
2023-12-05 11:30:42 -0800

    Fix runner on windows

--- src/runner/getProblemToRun.ts
+++ src/runner/getProblemToRun.ts
@@ -1,8 +1,13 @@
 import { getLastRunProblem } from "./lastRunProblem";
+import * as path from "path";
+
+const FS_DELIM = path.sep;
+
+const SRC_PROBLEMS = path.join("src", "problems") + FS_DELIM;
 
 export function getProblemToRun(inputArg: string | undefined): string {
-  if (inputArg != null && inputArg.startsWith("src/problems/")) {
-    return inputArg.slice(inputArg.lastIndexOf("/") + 1);
+  if (inputArg != null && inputArg.startsWith(SRC_PROBLEMS)) {
+    return inputArg.slice(inputArg.lastIndexOf(FS_DELIM) + 1);
   }
 
   if (inputArg != null && !Number.isNaN(Number(inputArg))) {
