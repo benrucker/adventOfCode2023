17395980e90b2cb09f27cad0c43ccebd8a732246
Ben Rucker <brucker117@gmail.com>
2023-12-11 21:28:37 -0800

    More partial day 5 part 2 stuff

--- src/problems/5/solution.ts
+++ src/problems/5/solution.ts
@@ -1,4 +1,5 @@
 import { Solution } from "../../types/Solution";
+import { Mutable } from "../../types/utility/Mutable";
 import { parseNumbers } from "../../utils/parseNumbers";
 import { split } from "../../utils/split";
 
@@ -21,18 +22,36 @@ interface Map {
 
 type MapByMapName = Record<string, Map>;
 
+interface Range {
+  readonly start: number;
+  /**
+   * The end of the range, EXCLUSIVE;
+   */
+  readonly end: number;
+}
+
 const findLowestLocation: Solution = (input): number => {
   const lines = input.split("\n");
   const seedsString = split(lines[0]! as SeedsInputString, ": ")[1];
 
-  const seedPairs = parseNumbers(seedsString);
-  const seeds = unrollSeedPairs(seedPairs);
+  const flatSeedNums = parseNumbers(seedsString);
+  const seedPairs = getSeedPairs(flatSeedNums);
+  const seedRanges = getSeedRanges(seedPairs);
 
+  const mapByMapName = parseMaps(lines);
+
+  let nextValues = findValidLocations(seedRanges, mapByMapName);
+
+  return Math.min(...nextValues.map(({ start }) => start));
+};
+
+function parseMaps(lines: string[]) {
   const mapByMapName: MapByMapName = {};
 
   let currentKeyString: string | undefined;
   let currentValueString: string | undefined;
   let currentValueByKey: Record<number, WithValueAndRange> | undefined;
+
   for (const line of lines.slice(2).concat([""])) {
     if (line.length === 0) {
       // stop parsing
@@ -64,15 +83,15 @@ const findLowestLocation: Solution = (input): number => {
     }
   }
 
-  let nextValues = findValidLocations(seeds, mapByMapName);
-  console.log(nextValues);
+  return mapByMapName;
+}
 
-  return Math.min(...nextValues);
-};
-
-function findValidLocations(seeds: number[], mapByMapName: MapByMapName) {
+function findValidLocations(
+  seedRanges: ReadonlyArray<Range>,
+  mapByMapName: MapByMapName,
+) {
   let nextMapName = "seed";
-  let nextValues: ReadonlyArray<number> = seeds;
+  let nextValues: ReadonlyArray<Range> = seedRanges;
   while (true) {
     const map = getNextMap(mapByMapName, nextMapName);
     const nextNameToProcess = map.valueName;
@@ -98,17 +117,99 @@ function getNextMap(mapByMapName: MapByMapName, mapName: string) {
   return map;
 }
 
-function findNextValues(map: Map, inputKeys: ReadonlyArray<number>) {
-  let nextValues: Array<number> = [];
+function findNextValues(map: Map, inputKeys: ReadonlyArray<Range>) {
+  const nextRanges: Array<Range> = [];
 
   for (const inputKey of inputKeys) {
-    nextValues.push(getNextValue(inputKey, map));
+    nextRanges.push(...getNextRanges(inputKey, map));
   }
 
-  return nextValues;
+  return nextRanges;
 }
 
-function getNextValue(inputKey: number, map: Map) {
+function getNextRanges(inputRange: Range, map: Map): ReadonlyArray<Range> {
+  const rangeLeft: Mutable<Range> = {
+    ...inputRange,
+  };
+
+  const output: Array<Range> = [];
+
+  // range: { start, end }
+  // while the input range start < end
+  while (rangeLeft.start < rangeLeft.end) {
+    const keyRangeStart = getKeyRangeStart(rangeLeft.start, map);
+    const keyRangeEnd = getKeyRangeEnd(keyRangeStart, map);
+    const outputRangeEnd = Math.min(keyRangeEnd, rangeLeft.end);
+
+    const valueRange = {
+      start: getValue(keyRangeStart.rangeStart, map),
+      end: getValue(outputRangeEnd, map),
+    };
+    output.push(valueRange);
+
+    console.log(`rangeLeft: ${rangeLeft.start} ${rangeLeft.end}`);
+    console.log(
+      `keyRangeStart: ${keyRangeStart.rangeStart} (from key ${keyRangeStart.key})`,
+    );
+    console.log(`keyRangeEnd: ${keyRangeEnd}`);
+    console.log(`outputRangeEnd: ${outputRangeEnd}`);
+    console.log(`valueRange: ${valueRange.start} ${valueRange.end}`);
+    console.log("----");
+
+    rangeLeft.start = outputRangeEnd;
+  }
+
+  return output;
+}
+
+function getKeyRangeStart(
+  inputStart: number,
+  map: Map,
+): { key: number; rangeStart: number } {
+  // Get the largest key smaller than inputStart
+  let maxKey = -1;
+  for (const key of map.keys) {
+    if (key > inputStart) {
+      break;
+    }
+    maxKey = key;
+  }
+
+  if (maxKey === -1) {
+    return { key: inputStart, rangeStart: inputStart };
+  }
+
+  const { range } = map.valueAndRangeByKey[maxKey]!;
+
+  const keyRangeEnd = maxKey + range - 1;
+
+  const mappedKey = inputStart;
+
+  if (inputStart > keyRangeEnd) {
+    return { key: inputStart, rangeStart: inputStart };
+  }
+
+  return { key: maxKey, rangeStart: inputStart };
+}
+
+/**
+ * Find the lowest range ending point for the given range start
+ */
+function getKeyRangeEnd({ key }: { key: number }, map: Map) {
+  const valueAndRange = map.valueAndRangeByKey[key];
+
+  if (valueAndRange == null) {
+    // Find next range start
+    const max = map.keys.find((mapKey) => mapKey > key);
+    return max ?? Number.POSITIVE_INFINITY;
+  }
+
+  const { range } = valueAndRange;
+
+  return key + range;
+}
+
+function getValue(inputKey: number, map: Map) {
   let maxKey = -1;
   for (const key of map.keys) {
     if (key > inputKey) {
@@ -140,25 +241,29 @@ function getNextValue(inputKey: number, map: Map) {
   return nextValue;
 }
 
-function unrollSeedPairs(seedPairs: ReadonlyArray<number>) {
+function getSeedPairs(seedPairs: ReadonlyArray<number>) {
   let seeds = [...seedPairs];
-  let output: Array<number> = [];
+  let output: Array<[number, number]> = [];
   while (seeds.length > 0) {
     const [seed, range] = seeds.slice(0, 2);
     if (seed == null || range == null) {
       throw new Error("Could not read seed input");
     }
-    output = output.concat(parseSeedPair(seed, range));
+    output.push([seed, range]);
     seeds = seeds.slice(2);
   }
   return output;
 }
 
-function parseSeedPair(seed: number, range: number) {
-  let seedNum = seed;
-  let output: Array<number> = [];
-  for (let i = 0; i < range; i++) {
-    output.push(seedNum + i);
+function getSeedRanges(
+  seedPairs: ReadonlyArray<[number, number]>,
+): ReadonlyArray<Range> {
+  const output: Array<Range> = [];
+  for (const [start, range] of seedPairs) {
+    output.push({
+      start,
+      end: start + range,
+    });
   }
   return output;
 }
