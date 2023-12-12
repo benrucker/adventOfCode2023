68ae64b6c6dd423b06ef0111c579f6fc69b81c70
Ben Rucker <brucker117@gmail.com>
2023-12-11 21:28:23 -0800

    Unescape double backslashes

--- src/runner/getProblemToRun.ts
+++ src/runner/getProblemToRun.ts
@@ -6,6 +6,8 @@ const FS_DELIM = path.sep;
 const SRC_PROBLEMS = path.join("src", "problems") + FS_DELIM;
 
 export function getProblemToRun(inputArg: string | undefined): string {
+  inputArg = inputArg?.replace(/\\\\/g, "\\");
+
   if (inputArg != null && inputArg.startsWith(SRC_PROBLEMS)) {
     return inputArg.slice(inputArg.lastIndexOf(FS_DELIM) + 1);
   }
